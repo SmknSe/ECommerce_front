07aef3fed07594b1e83f4eae5ad918bd2206da2f
SmknSe <semeykin2003@mail.ru>
2024-05-10 23:48:35 +0300

    url_change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = 'http://localhost:8080/api/'
+const BASE_URL = 'https://ecommerce-1dbp.onrender.com/api/'
 
 export default axios.create({
     baseURL: BASE_URL
